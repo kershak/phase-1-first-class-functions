d6c1dfe8f3dc214ea3e8dc4a33bde7ecd834d124
Nills <yakusaz17@gmail.com>
2024-08-14 08:49:38 -0400

    completed

--- index.js
+++ index.js
@@ -3,15 +3,17 @@ function receivesAFunction (callback){
     callback();
 }
 
+
 function returnsANamedFunction() {
   return function named() {
     console.log("Someone named Emily")
   }
 }
 
+
 function returnsAnAnonymousFunction (name){
-    return function() {
-        console.log('Anonymous');
-      }
+  return function() {
+      console.log('Anonymous');
     }
-    
+  }
+  
